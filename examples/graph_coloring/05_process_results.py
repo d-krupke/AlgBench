e98f8aa204f4171d0bdc885e2a2d5382904c32da
Dominik Krupke <krupke@ibr.cs.tu-bs.de>
2023-05-25 16:08:25 +0200

    documentation of example

--- examples/graph_coloring/05_process_results.py
+++ examples/graph_coloring/05_process_results.py
@@ -1,7 +1,11 @@
+"""
+This script simplifies the raw data to a small pandas table.
+"""
 from algbench import read_as_pandas
 
 if __name__=="__main__":
-    t = read_as_pandas("./03_benchmark_data/", lambda result: {"instance": result["parameters"]["args"]["instance_name"],
+    t = read_as_pandas("./03_benchmark_data/",
+                        lambda result: {"instance": result["parameters"]["args"]["instance_name"],
                                                        "strategy":  result["parameters"]["args"]["alg_params"]["strategy"],
                                                        "interchange": result["parameters"]["args"]["alg_params"].get("interchange", None),
                                                        "colors": result["data"]["result"]["n_colors"],
